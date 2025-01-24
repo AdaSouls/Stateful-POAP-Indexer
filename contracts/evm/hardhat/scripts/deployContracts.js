9e41e1baeb5486a094a0cadae82de190ea8a14e6
Matias Falcone <matias.falcone@gmail.com>
2025-01-24 12:55:13 +1100

    updated deployContracts script and modified docker compose commands syntax

--- contracts/evm/hardhat/scripts/deployContracts.js
+++ contracts/evm/hardhat/scripts/deployContracts.js
@@ -1,5 +1,5 @@
-const yaml = require('js-yaml');
-const fs = require('fs');
+const yaml = require("js-yaml");
+const fs = require("fs");
 const parameters = require("../parameters.json");
 const path = require("path");
 const hre = require("hardhat");
@@ -7,25 +7,36 @@ const hre = require("hardhat");
 const getBlockNumber = async () => {
   const blockNumber = await hre.ethers.provider.getBlockNumber();
   return blockNumber;
-}
+};
 
 async function main() {
   const accounts = await ethers.getSigners();
   // Deploy Poap
   const Poap = await hre.ethers.getContractFactory("Poap");
-  const poap = await Poap.deploy(parameters.Poap.name, parameters.Poap.symbol, accounts[0].address);
-  
+  const poap = await Poap.deploy(
+    parameters.Poap.name,
+    parameters.Poap.symbol,
+    accounts[0].address
+  );
+
   await poap.waitForDeployment();
   await poap["initialize(string,address[])"](parameters.Poap.baseUri, []);
 
-  const newContractAddress = await poap.getAddress()
+  const newContractAddress = await poap.getAddress();
   console.log("Poap deployed and initialized at: ", newContractAddress);
-  
+
   const newBlockHeight = await getBlockNumber();
 
+  console.log(newBlockHeight);
+
   // I have to update extensions.yml
-  let doc = yaml.load(fs.readFileSync(path.resolve(__dirname, "../../../../extensions.yml"), 'utf-8'));
-  
+  let doc = yaml.load(
+    fs.readFileSync(
+      path.resolve(__dirname, "../../../../extensions.yml"),
+      "utf-8"
+    )
+  );
+
   doc.extensions[0].contractAddress = newContractAddress;
   doc.extensions[1].contractAddress = newContractAddress;
   doc.extensions[2].contractAddress = newContractAddress;
@@ -34,9 +45,12 @@ async function main() {
   doc.extensions[1].startBlockHeight = newBlockHeight;
   doc.extensions[2].startBlockHeight = newBlockHeight;
 
-  fs.writeFileSync('extensions.yml', yaml.dump(doc, {
-    forceQuotes: true  
-  }));
+  fs.writeFileSync(
+    "extensions.yml",
+    yaml.dump(doc, {
+      forceQuotes: true,
+    })
+  );
 
   // Verify the contract after deploying
   /*await hre.run("verify:verify", {
@@ -47,7 +61,6 @@ async function main() {
       accounts[0].address,
     ],
   });*/
-  
 }
 
 main();
